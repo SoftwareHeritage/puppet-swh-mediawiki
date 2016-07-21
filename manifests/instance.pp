d5e4d8814609346b108a6afed0238f7c7b9a8154
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-07-21 16:35:18 +0200

    Permit to differentiate logo between instances
    Related T503

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -17,6 +17,7 @@ define mediawiki::instance (
   String $db_password = 'verysecret',
   String $secret_key = 'secretkey',
   String $upgrade_key = 'upgradekey',
+  String $swh_logo = '/images/b/b2/swh-intranet-logo.png',
   ){
     include ::mediawiki
 
